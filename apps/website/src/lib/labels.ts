982306a81c769c89ff2b37a9cf7a3accbff93a06
metamethods <tabledmetamethods@gmail.com>
2025-11-07 23:37:01 -0800

    feat(website): add x-verse image

--- apps/website/src/lib/labels.ts
+++ apps/website/src/lib/labels.ts
@@ -7,7 +7,8 @@ const gameVersionLabels: { range: string; label: string }[] = [
 	{ range: '>=2.15.x <=2.16.x', label: 'sun_plus' },
 	{ range: '>=2.20.x <=2.22.x', label: 'luminous' },
 	{ range: '>=2.25.x <=2.27.x', label: 'luminous_plus' },
-	{ range: '2.30.x', label: 'verse' }
+	{ range: '2.30.x', label: 'verse' },
+	{ range: '2.40.x', label: 'x_verse' },
 ];
 
 export function getGameVersionLabel(version: string): string | undefined {
